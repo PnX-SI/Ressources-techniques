1ee8042265511d5d61a288f87a2c223f95f04fdd
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2018-10-08 11:23:33 +0200

    Update obs_zonage.sql

--- GeoNature/V1/obs_zonage.sql
+++ GeoNature/V1/obs_zonage.sql
@@ -22,3 +22,22 @@ SELECT DISTINCT cd_nom, nom_vern, nom_valide, count(id_synthese) AS nb_obs
 FROM synthese.v_flore_ril
 GROUP BY cd_nom, nom_vern, nom_valide
 ORDER BY nom_valide;
+
+-------------------------------------------------------
+-- On peut aussi le faire en une seule requête sans vue 
+
+WITH mydata AS (  
+  SELECT s.id_synthese,
+    s.cd_nom,
+    t.nom_vern,
+    t.nom_valide, 
+    s.the_geom_local
+   FROM synthese.syntheseff s
+     JOIN layers.l_zonesstatut l ON st_intersects(l.the_geom, s.the_geom_local)
+     JOIN taxonomie.taxref t ON t.cd_nom = s.cd_nom
+  WHERE l.id_zone = 3606 AND t.regne::text = 'Plantae'::text AND ST_area(s.the_geom_local) < 1000000
+)
+SELECT DISTINCT cd_nom, nom_vern, nom_valide, count(*) AS nb_obs
+FROM mydata
+GROUP BY cd_nom, nom_vern, nom_valide
+ORDER BY nom_valide;
