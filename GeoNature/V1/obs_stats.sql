df3759f4d1fbd76da0621f729b00adb093d29e02
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2018-12-03 09:53:47 +0100

    Ajout stats par lot, programme, regne

--- GeoNature/V1/obs_stats.sql
+++ GeoNature/V1/obs_stats.sql
@@ -26,3 +26,36 @@ WITH nby AS
  AND EXTRACT('year' FROM dateobs) < 2012
 ) 
 SELECT count(*) FROM nby
+
+-- nb d'obs par lot de donnees
+SELECT count(*) nb_data, l.nom_lot
+FROM synthese.syntheseff s
+JOIN meta.bib_lots l ON l.id_lot = s.id_lot
+WHERE supprime = false
+GROUP BY l.nom_lot
+ORDER BY nb_data desc;
+
+-- nb d'obs par programme
+SELECT count(*) nb_data, p.nom_programme, p.actif
+FROM synthese.syntheseff s
+JOIN meta.bib_lots l ON l.id_lot = s.id_lot
+JOIN meta.bib_programmes p ON p.id_programme = l.id_programme
+WHERE supprime = false
+GROUP BY p.nom_programme, p.actif
+ORDER BY nb_data desc;
+
+-- nb d'obs par regne
+SELECT count(*) nb_data, t.regne
+FROM synthese.syntheseff s
+JOIN taxonomie.taxref t ON t.cd_nom = s.cd_nom
+WHERE s.supprime = false
+GROUP BY t.regne;
+
+-- nb de taxons par regne
+SELECT count(*), a.regne FROM(
+SELECT count(*) nb_data, t.regne, t.cd_nom
+FROM synthese.syntheseff s
+JOIN taxonomie.taxref t ON t.cd_nom = s.cd_nom
+WHERE s.supprime = false
+GROUP BY t.regne, t.cd_nom) a
+GROUP BY a.regne;
