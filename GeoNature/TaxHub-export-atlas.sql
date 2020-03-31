90b07374a42599424da5577e3defbd4439113123
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2020-03-31 23:57:18 +0200

    Update TaxHub-export-atlas.sql

--- GeoNature/TaxHub-export-atlas.sql
+++ GeoNature/TaxHub-export-atlas.sql
@@ -1,9 +1,10 @@
--- Import des attributs et ajout de la source pour Description et Commentaire
+-- Export des attributs de l'atlas et ajout de la source pour Description et Commentaire
 
-SELECT cor_taxon_attribut.cd_ref,
+SELECT 
+    cor_taxon_attribut.cd_ref,
     cor_taxon_attribut.id_attribut,
-    CASE WHEN (cor_taxon_attribut.id_attribut = 100) OR (cor_taxon_attribut.id_attribut = 101) THEN CONCAT(cor_taxon_attribut.valeur_attribut,' <i>Source : Parc national des Ecrins</i>')
-    ELSE cor_taxon_attribut.valeur_attribut
+       CASE WHEN (cor_taxon_attribut.id_attribut = 100) OR (cor_taxon_attribut.id_attribut = 101) THEN CONCAT(cor_taxon_attribut.valeur_attribut,' <i>Source : Parc national des Ecrins</i>')
+       ELSE cor_taxon_attribut.valeur_attribut
     END AS value
    FROM taxonomie.cor_taxon_attribut
   WHERE cor_taxon_attribut.id_attribut = ANY (ARRAY[100, 101, 102, 103])
