9729d5035bf804eda550b07dea591ff8a5274420
joel.clement <joelclems@gmail.com>
2020-10-20 18:40:52 +0200

    up synthese speed

--- GeoNature/migration/sicen_to_occtaxV2/data/insert/before_insert.sql
+++ GeoNature/migration/sicen_to_occtaxV2/data/insert/before_insert.sql
@@ -16,6 +16,8 @@ ALTER TABLE utilisateurs.t_roles ADD COLUMN IF NOT EXISTS id_personne INTEGER;
 
 ALTER TABLE pr_occtax.cor_counting_occtax DISABLE TRIGGER tri_insert_synthese_cor_counting_occtax;
 
+
+ALTER TABLE gn_synthese.cor_area_synthese DISABLE TRIGGER tri_maj_cor_area_taxon;
 ALTER TABLE gn_synthese.synthese DISABLE TRIGGER tri_del_area_synt_maj_corarea_tax;
 ALTER TABLE gn_synthese.synthese DISABLE TRIGGER tri_insert_cor_area_synthese;
 ALTER TABLE gn_synthese.synthese DISABLE TRIGGER tri_update_cor_area_taxon_update_cd_nom;
