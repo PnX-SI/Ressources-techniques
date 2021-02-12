15aeeb108f22285b2bedd11b75b810b02cc3d658
joel.clement <joelclems@gmail.com>
2021-02-12 19:55:07 +0100

    remote config

--- GeoNature/parcs/scripts/get_remote_config.sh
+++ GeoNature/parcs/scripts/get_remote_config.sh
@@ -15,8 +15,8 @@ get geonature/config/geonature_config.toml -o remote_config/${parc}/geonature_co
 get atlas/atlas/configuration/settings.ini -o remote_config/${parc}/settings_atlas.ini;
 get atlas/atlas/configuration/config.py -o remote_config/${parc}/config_atlas.py;
 
-get geonature/backend/static/mobile/occtax/settings.json -o remote_config_/${parc}/settings_occtax.json;
-get geonature/backend/static/mobile/sync/settings.json -o remote_config_/${parc}/settings_sync.json;
+get /geonature/backend/static/mobile/occtax/settings.json -o remote_config/${parc}/settings_occtax.json;
+get /geonature/backend/static/mobile/sync/settings.json -o remote_config/${parc}/settings_sync.json;
 
 bye
 "
