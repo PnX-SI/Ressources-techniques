539bd459d432b768fa563ac234cb3f1cdc972807
audrey_en_guyane <audrey.thonnel@guyane-parcnational.fr>
2021-03-18 15:11:10 -0300

    all

--- GeoNature/PNX/process/pag/data/open_fdw.sql
+++ GeoNature/PNX/process/pag/data/open_fdw.sql
@@ -1,17 +1,15 @@
+DROP SCHEMA IF EXISTS v1_compat CASCADE;
+CREATE SCHEMA v1_compat;
 CREATE EXTENSION IF NOT EXISTS postgres_fdw;
 
 DROP SERVER IF EXISTS geonature1server CASCADE;
 CREATE SERVER geonature1server  
       FOREIGN DATA WRAPPER postgres_fdw 
       OPTIONS (host :'db_host_v1', dbname :'db_name_v1', port :'db_port_v1');
-
 CREATE USER MAPPING  
         FOR :user_pg
         SERVER geonature1server
         OPTIONS (password :'user_pg_pass_v1',user :'user_pg_v1');
-
-DROP SCHEMA IF EXISTS v1_compat CASCADE;
-CREATE SCHEMA v1_compat;
 
 IMPORT FOREIGN SCHEMA utilisateurs FROM SERVER geonature1server INTO v1_compat;
 IMPORT FOREIGN SCHEMA synthese FROM SERVER geonature1server INTO v1_compat;
