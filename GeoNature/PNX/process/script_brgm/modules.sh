357927dff195a8df11fe980038d8e1364ded8228
joel.clement <joelclems@gmail.com>
2021-04-27 13:43:46 +0200

    master

--- GeoNature/PNX/process/script_brgm/modules.sh
+++ GeoNature/PNX/process/script_brgm/modules.sh
@@ -1,4 +1,17 @@
-path_modules=/geonature/
+# le module export nécessite une configuration OK pour les mails
+# dans geonature_config.toml
+#
+#
+# [MAIL_CONFIG]
+#         MAIL_SERVER = "mail.brgm.fr"
+#         MAIL_PORT = 25
+#         MAIL_USE_TLS = false
+#         MAIL_USE_SSL = false
+#         MAIL_USERNAME = ""
+#         MAIL_PASSWORD = ""
+#         MAIL_DEFAULT_SENDER = "brunolafage@parcnational.fr"
+
+
 
 git clone https://github.com/PnX-SI/gn_module_import /geonature/import
 git clone https://github.com/PnX-SI/gn_module_export /geonature/export
@@ -7,11 +20,10 @@ git clone https://github.com/PnX-SI/gn_module_monitoring /geonature/monitoring
 source /geonature/geonature/backend/venv/bin/activate
 
 geonature install_gn_module /geonature/import "import" --build=false
-
 geonature install_gn_module /geonature/export "export" --build=false
-
 geonature install_gn_module /geonature/monitoring "monitoring"
-# à faire seulement pour la recette?
+
+# à faire seulement pour la recette
 flask monitorings install /geonature/monitoring/contrib/test
 
 deactivate
