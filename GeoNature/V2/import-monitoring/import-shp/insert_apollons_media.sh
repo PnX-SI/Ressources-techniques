b7211b34e0cb9e6ead788a25e481f7b16377328c
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2021-07-23 10:06:08 +0200

    Create insert_apollons_media.sh

---
+++ GeoNature/V2/import-monitoring/import-shp/insert_apollons_media.sh
@@ -0,0 +1,182 @@
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_7\ \(Cros\ du\ Lac\)/CM_CL_99.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_146.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_10\ \(Hom\)/CM_LH_158.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CS_PA_121.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_6\ \(La\ Combette\)/CM_LC_76.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_4\ \(Cazeneuve\)/CM_CA_25.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_5\ \(Serre\ de\ Lattes\)/CM_SL_57.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_7\ \(Cros\ du\ Lac\)/CM_CL_103.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_132.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CS_PA_115.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_5\ \(Serre\ de\ Lattes\)/CM_SL_51.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_2\ \(La\ Paliere\)/CM_PA_15.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CS_PA_105.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_6\ \(La\ Combette\)/CM_LC_89.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_3\ \(Nivoliers\)/CM_NI_21.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_136.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_133.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CS_PA_127.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_6\ \(La\ Combette\)/CM_LC_90.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CS_PA_112.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_9\ \(Gaoujac\)/CM_GA_37.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_5\ \(Serre\ de\ Lattes\)/CM_SL_50.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_7\ \(Cros\ du\ Lac\)/CM_CL_101.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_5\ \(Serre\ de\ Lattes\)/CM_SL_61.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station\ _1\ \(Cros\ altier\)/CM_CA_08.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CS_PA_119.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_8\ \(Serre\ de\ la\ Rastou\)/CM_SR_67.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_7\ \(Cros\ du\ Lac\)/CM_CL_102.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_151.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_8\ \(Serre\ de\ la\ Rastou\)/CM_SR_65.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_8\ \(Serre\ de\ la\ Rastou\)/CM_SR_71.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_144.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_4\ \(Cazeneuve\)/CM_CA_31.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_6\ \(La\ Combette\)/CM_LC_85.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CS_PA_114.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_6\ \(La\ Combette\)/CM_LC_94.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_145.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_6\ \(La\ Combette\)/CM_LC_80.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_6\ \(La\ Combette\)/CM_LC_91.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_5\ \(Serre\ de\ Lattes\)/CM_SL_60.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_149.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_10\ \(Hom\)/CM_HL_163.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_131.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_5\ \(Serre\ de\ Lattes\)/CM_SL_53.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_150.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_6\ \(La\ Combette\)/CM_LC_87.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CM_PA_166.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CS_PA_120.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_5\ \(Serre\ de\ Lattes\)/CM_SL_55.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_148.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_10\ \(Hom\)/CM_LH_155.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_3\ \(Nivoliers\)/CM_NI_17.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_10\ \(Hom\)/CM_LH_164.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_8\ \(Serre\ de\ la\ Rastou\)/CM_SR_69.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_4\ \(Cazeneuve\)/CM_CA_29.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_140.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CS_PA_116.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_5\ \(Serre\ de\ Lattes\)/CM_SL_56.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_128.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_4\ \(Cazeneuve\)/CM_CA_23.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_8\ \(Serre\ de\ la\ Rastou\)/CM_SR_64.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_3\ \(Nivoliers\)/CM_NI_20.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_8\ \(Serre\ de\ la\ Rastou\)/CM_SR_73.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_6\ \(La\ Combette\)/CM_LC_92.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_9\ \(Gaoujac\)/CM_GA_41.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_6\ \(La\ Combette\)/CM_LC_78.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_8\ \(Serre\ de\ la\ Rastou\)/CM_SR_74.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_9\ \(Gaoujac\)/CM_GA_40.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_10\ \(Hom\)/CM_LH_159.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station\ _1\ \(Cros\ altier\)/CM_CA_03.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_5\ \(Serre\ de\ Lattes\)/CM_SL_46.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station\ _1\ \(Cros\ altier\)/CM_CA_01.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_135.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_134.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station\ _1\ \(Cros\ altier\)/CM_CA_06.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CS_PA_111.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_4\ \(Cazeneuve\)/CM_CA_27.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_6\ \(La\ Combette\)/CM_LC_81.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_4\ \(Cazeneuve\)/CM_CA_32.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_9\ \(Gaoujac\)/CM_GA_33.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CS_PA_122.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_9\ \(Gaoujac\)/CM_GA_36.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CS_PA_124.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CM_PA_168.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_5\ \(Serre\ de\ Lattes\)/CM_SL_59.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_8\ \(Serre\ de\ la\ Rastou\)/CM_SR_68.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_9\ \(Gaoujac\)/CM_GA_39.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_138.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_142.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_7\ \(Cros\ du\ Lac\)/CM_CL_98.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_5\ \(Serre\ de\ Lattes\)/CM_SL_62.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_5\ \(Serre\ de\ Lattes\)/CM_SL_58.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_5\ \(Serre\ de\ Lattes\)/CM_SL_44.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_10\ \(Hom\)/CM_LH_160.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_2\ \(La\ Paliere\)/CM_PA_12.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_141.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CS_PA_123.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_143.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_6\ \(La\ Combette\)/CM_LC_79.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_9\ \(Gaoujac\)/CM_GA_35.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_10\ \(Hom\)/CM_LH_153.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CS_PA_106.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CS_PA_117.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_7\ \(Cros\ du\ Lac\)/CM_CL_100.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_2\ \(La\ Paliere\)/CM_PA_16.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_5\ \(Serre\ de\ Lattes\)/CM_SL_47.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_6\ \(La\ Combette\)/CM_LC_83.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_10\ \(Hom\)/CM_LH_157.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_4\ \(Cazeneuve\)/CM_CA_24.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_8\ \(Serre\ de\ la\ Rastou\)/CM_SR_66.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_4\ \(Cazeneuve\)/CM_CA_30.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station\ _1\ \(Cros\ altier\)/CM_CA_02.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_2\ \(La\ Paliere\)/CM_PA_14.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station\ _1\ \(Cros\ altier\)/CM_CA_07.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_10\ \(Hom\)/CM_LH_156.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CS_PA_110.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CM_PA_167.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CS_PA_107.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_5\ \(Serre\ de\ Lattes\)/CM_SL_54.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_129.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_5\ \(Serre\ de\ Lattes\)/CM_SL_43.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_5\ \(Serre\ de\ Lattes\)/CM_SL_44.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_3\ \(Nivoliers\)/CM_NI_22.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_6\ \(La\ Combette\)/CM_LC_88.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_152.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_7\ \(Cros\ du\ Lac\)/CM_CL_97.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_9\ \(Gaoujac\)/CM_GA_38.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_9\ \(Gaoujac\)/CM_GA_34.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_137.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_6\ \(La\ Combette\)/CM_LC_77.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_4\ \(Cazeneuve\)/CM_CA_28.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_3\ \(Nivoliers\)/CM_NI_19.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_5\ \(Serre\ de\ Lattes\)/CM_SL_48.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_10\ \(Hom\)/CM_LH_162.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_2\ \(La\ Paliere\)/CM_PA_13.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station\ _1\ \(Cros\ altier\)/CM_CA_04.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CS_PA_118.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_7\ \(Cros\ du\ Lac\)/CM_CL_95.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CS_PA_113.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_7\ \(Cros\ du\ Lac\)/CM_CL_96.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CS_PA_108.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station\ _1\ \(Cros\ altier\)/CM_CA_10.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_4\ \(Cazeneuve\)/CM_CA_165.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_6\ \(La\ Combette\)/CM_LC_82.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_8\ \(Serre\ de\ la\ Rastou\)/CM_SR_63.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_4\ \(Cazeneuve\)/CM_CA_26.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CS_PA_126.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CS_PA_125.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_3\ \(Nivoliers\)/CM_NI_18.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_5\ \(Serre\ de\ Lattes\)/CM_SL_52.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_10\ \(Hom\)/CM_LH_161.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_147.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_8\ \(Serre\ de\ la\ Rastou\)/CM_SR_72.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_2\ \(La\ Paliere\)/CM_PA_11.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CS_PA_109.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_10\ \(Hom\)/CM_LH_154.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_6\ \(La\ Combette\)/CM_LC_84.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_139.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station\ _1\ \(Cros\ altier\)/CM_CA_09.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_6\ \(La\ Combette\)/CM_LC_93.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_6\ \(La\ Combette\)/CM_LC_75.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_5\ \(Serre\ de\ Lattes\)/CM_SL_45.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_5\ \(Serre\ de\ Lattes\)/CM_SL_49.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station\ _1\ \(Cros\ altier\)/CM_CA_05.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_6\ \(La\ Combette\)/CM_LC_86.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_12\ \(Champerboux\)/CS_CH_130.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_8\ \(Serre\ de\ la\ Rastou\)/CM_SR_70.JPG /tmp/photos_dalles
+ cp photo_dalle_orpins_apollon/Photo_selectionnees/Station_11\ \(Paros\)/CS_PA_104.JPG /tmp/photos_dalles
+ 
+ 
+#Copy
+ mkdir /tmp/photos_dalles_c
+ cd /tmp/photos_dalles
+ for f in *.JPG; do
+    cp  "$f" ../photos_dalles_c/dalle_"${f}"
+done
+
+
+#Compress
+cd /tmp/photos_dalles_c
+jpegoptim --size=2000 *
+cp * ~/dev/GeoNature/backend/static/medias/2/
