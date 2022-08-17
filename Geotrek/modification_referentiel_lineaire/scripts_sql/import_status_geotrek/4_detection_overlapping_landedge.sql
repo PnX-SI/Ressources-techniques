455e83071547b0c91c1e2b5cf1c381b888d8a126
Idrissa Djepa Creutz <73114156+IdrissaD@users.noreply.github.com>
2022-08-17 14:27:52 +0200

    Update 4_detection_overlapping_landedge.sql

--- Geotrek/modification_referentiel_lineaire/scripts_sql/import_status_geotrek/4_detection_overlapping_landedge.sql
+++ Geotrek/modification_referentiel_lineaire/scripts_sql/import_status_geotrek/4_detection_overlapping_landedge.sql
@@ -1,4 +1,6 @@
 ---------- Création d'une table permettant de visualiser les erreurs de landedge dans QGIS sous forme ponctuelle
+
+DROP TABLE IF EXISTS overlapping_landedge;
 CREATE TABLE IF NOT EXISTS overlapping_landedge (
     id integer PRIMARY KEY,
     geom geometry,
