78161cfb153aa8f3595763414b27cc61597733ad
PNPyrenees <85548796+PNPyrenees@users.noreply.github.com>
2024-07-03 12:30:41 +0200

    Update gn2pg_install.sql
    Amélioration de la gestion du cas ou l'API "GN-Exports" retourne un résultat vide

--- GeoNature/gn2pg/gn2pg_install.sql
+++ GeoNature/gn2pg/gn2pg_install.sql
@@ -25,7 +25,6 @@ CREATE OR REPLACE FUNCTION gn2pg.gn2pg(
     VOLATILE PARALLEL UNSAFE
 AS $BODY$
 DECLARE http_request text;
---DECLARE token text;
 DECLARE tmp_cols text;
 DECLARE cols text[];
 DECLARE loop_index integer :=0;
@@ -56,43 +55,49 @@ BEGIN
 		
 		-- Exec curl command and write json result in temporary table
 		EXECUTE 'COPY tmp_export_' || destination_table || ' FROM PROGRAM ''' || http_request || ''' CSV QUOTE E''\x01'' DELIMITER E''\x02'';';
-		
-		-- First loop ?
-		IF loop_index = 0 THEN
-			/* Get list of column and force type to text */
-			EXECUTE FORMAT('
-				SELECT string_agg(column_name, '' TEXT, '') || '' TEXT'' 
-				FROM (
-					SELECT json_object_keys(((data->>''items'')::json->0)::json) as column_name FROM tmp_export_%I a
-				) b'
-				, destination_table
-			) INTO tmp_cols;
-		
-			/* Create table and insert data */
-			EXECUTE FORMAT('
-				CREATE TABLE %I.%I AS 
-				SELECT x.* 
-				FROM 
-					tmp_export_%I
-					CROSS JOIN LATERAL json_to_recordset((data->>''items'')::json) as x(' || tmp_cols || ')'
-				,destination_schema, destination_table, destination_table
-			);
-		
-		ELSE
-			-- Other, just INSERT data 
-			EXECUTE FORMAT('
-				INSERT INTO %I.%I 
-				SELECT x.* 
-				FROM 
-					tmp_export_%I
-					CROSS JOIN LATERAL json_to_recordset((data->>''items'')::json) as x(' || tmp_cols || ')'
-				,destination_schema, destination_table, destination_table
-			);
-		END IF;
-		
+
 		-- get number of exported data
 		EXECUTE FORMAT('SELECT json_array_length((data->>''items'')::json) FROM tmp_export_%I', destination_table) INTO nb_data_exported;
 		
+		-- Check if API request return data
+		IF loop_index = 0 and nb_data_exported = 0 THEN
+			RAISE NOTICE 'gn2pg : The request return 0 data';
+		ELSE 
+			-- First loop ?
+			IF loop_index = 0 THEN
+				/* Get list of column and force type to text */
+				EXECUTE FORMAT('
+					SELECT string_agg(column_name, '' TEXT, '') || '' TEXT'' 
+					FROM (
+						SELECT json_object_keys(((data->>''items'')::json->0)::json) as column_name FROM tmp_export_%I a
+					) b'
+					, destination_table
+				) INTO tmp_cols;
+			
+				/* Create table and insert data */
+				EXECUTE FORMAT('
+					CREATE TABLE %I.%I AS 
+					SELECT x.* 
+					FROM 
+						tmp_export_%I
+						CROSS JOIN LATERAL json_to_recordset((data->>''items'')::json) as x(' || tmp_cols || ')'
+					,destination_schema, destination_table, destination_table
+				);
+			
+			ELSE
+				-- Other, just INSERT data 
+				EXECUTE FORMAT('
+					INSERT INTO %I.%I 
+					SELECT x.* 
+					FROM 
+						tmp_export_%I
+						CROSS JOIN LATERAL json_to_recordset((data->>''items'')::json) as x(' || tmp_cols || ')'
+					,destination_schema, destination_table, destination_table
+				);
+			END IF;
+
+		END IF;
+			
 		-- If less than 1000 recard -> end of GN-View
 		IF nb_data_exported < 1000 THEN
 			EXIT;  -- exit loop
@@ -110,9 +115,8 @@ EXCEPTION
 			_message := MESSAGE_TEXT,
 			_context := PG_EXCEPTION_CONTEXT;
 			
-	INSERT INTO gn2pg.log (date, statut, message) VALUES (TIMEOFDAY()::timestamp, 'error', 'Error during downloading datas for exports #' || export_id || ' from ' || gn_domain || 'with message : ' || _message || ' - ' || _context);
+	INSERT INTO gn2pg.log (date, statut, message) VALUES (TIMEOFDAY()::timestamp, 'error', 'Error during downloading datas for exports #' || export_id || ' from ' || gn_domain || ' with message : ' || _message || ' - ' || _context);
 
 END;
 $BODY$;
-
 
